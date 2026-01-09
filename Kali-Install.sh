6ef4710a4cbf5e1982366935736e76c10c14dd09
LB <144353262+LabRoboticist@users.noreply.github.com>
2026-01-09 16:12:48 -0600

    Update Kali-Install.sh

--- Kali-Install.sh
+++ Kali-Install.sh
@@ -16,3 +16,5 @@ done
 #use one of these to check to see if the package is already installed on this machine
   #apt list --installed "$pkg" 2>/dev/null | grep -q "^$pkg/"
   #dpkg -s "$pkg" >/dev/null 2>&1 #dpkg is debian's package manager
+
+#This comment was made with my phone :)
