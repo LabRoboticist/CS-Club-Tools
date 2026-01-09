e2493e4074ff6525de05327ba1a794ee8bd8448b
LB <144353262+LabRoboticist@users.noreply.github.com>
2026-01-09 15:14:31 -0600

    Update echo statement for package installation message

--- Kali-Install.sh
+++ Kali-Install.sh
@@ -3,4 +3,4 @@ echo "Only people with Root level permissions will be able to"
 echo "This script is to be used on Debian systems only, and preferabley on Kali Linux"
 
 packages=(nmap vim nano)
-echo "${packages[@]}"
+echo "The packages being instaled are ${packages[@]}."
