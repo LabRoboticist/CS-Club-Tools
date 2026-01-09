527464c5765b6f914b219158b58dc08cc69873dd
LB <144353262+LabRoboticist@users.noreply.github.com>
2026-01-09 14:48:26 -0600

    Enhance script with user messages and package array
    Add echo statements for user guidance and package list.

--- Kali-Install.sh
+++ Kali-Install.sh
@@ -1,3 +1,6 @@
 
+echo "Only people with Root level permissions will be able to 
 echo "This script is to be used on Debian systems only, and preferabley on Kali Linux"
 
+packages = (nmap vim nano)
+echo "${packages[@]}"
