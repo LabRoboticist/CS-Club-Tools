bcd60b8eb8c9a0a9e72c5789be2451c06a1ab2ce
LB <144353262+LabRoboticist@users.noreply.github.com>
2026-01-09 15:19:07 -0600

    Comment out package installation logic
    Comment out package installation messages and loop.

--- Kali-Install.sh
+++ Kali-Install.sh
@@ -2,5 +2,9 @@
 echo "Only people with Root level permissions will be able to"
 echo "This script is to be used on Debian systems only, and preferabley on Kali Linux"
 
-packages=(nmap vim nano)
-echo "The packages being instaled are ${packages[@]}."
+#packages=(nmap vim nano)
+#echo "The packages being instaled are ${packages[@]}."
+
+for i in ${packages[@]}; do
+  echo $i
+done
