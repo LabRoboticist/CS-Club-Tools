5e86ebfe834693759c51bb0aafd5356bef523c26
LB <144353262+LabRoboticist@users.noreply.github.com>
2026-01-10 08:24:49 -0600

    Update Kali-Install.sh

--- Kali-Install.sh
+++ Kali-Install.sh
@@ -3,6 +3,7 @@ echo "Only people with Root level permissions will be able to"
 echo "This script is to be used on Debian systems only, and preferabley on Kali Linux"
 
 packages=(nmap vim nano)
+#packages to be installed: rpi-imager 
 #echo "The packages being instaled are ${packages[@]}."
 
 for i in "${packages[@]}"; do
